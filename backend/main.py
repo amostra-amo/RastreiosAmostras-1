b0f51b55b86bccb77a425cf91ec3f619542352d4
amostra-amo <amostra@quatrok.com.br>
2026-09-22 11:26:12 -0300

    Restrict CORS origins to specific domains

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "http://rastreioamostra.quatrok.com.br",
+        "https://rastreioamostra.quatrok.com.br",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
